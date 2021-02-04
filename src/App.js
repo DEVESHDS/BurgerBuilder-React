f8c83b120f19fbd165d04c43ce13834a4fda7930
DEVESHDS <dev1710.sharma@gmail.com>
2021-02-04 14:51:51 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Layout from "./Components/HOC/Layout/Layout";
 import BurgerBuilder from "./Components/Containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./Components/Containers/CheckOut/checkout";
+import Orders from "./Components/Containers/Orders/Orders";
+import Auth from "./Components/Containers/Auth/Auth";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/Orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
