ad8a4216c673c242a200eaf71199a6b893b4db1e
DEVESHDS <dev1710.sharma@gmail.com>
2021-02-06 18:24:57 +0530

    CSS for button added

--- src/Components/UI/button/button.js
+++ src/Components/UI/button/button.js
@@ -3,6 +3,7 @@ import classes from "./button.module.css";
 
 const button = (props) => (
   <button
+    disabled={props.disabled}
     className={[classes.button, classes[props.btntype]].join(" ")}
     onClick={props.clicked}
   >
