528337c387f6ff8e8a11adba5a27f5063c6ba2c9
DEVESHDS <dev1710.sharma@gmail.com>
2021-02-07 23:49:51 +0530

    testing of components added

---
+++ src/Components/Containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { BurgerBuilder } from "./BurgerBuilder";
+import BuildControls from "../../Burger/BuildControls/BuildControls";
+
+configure({ adapter: new Adapter() });
+
+describe("Test for Burger Builder Component", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<BurgerBuilder onInitIngredient={() => {}} />);
+  });
+  it("should have BuildControls when receiving ingredients", () => {
+    wrapper.setProps({ ings: { salad: 0 } });
+    expect(wrapper.find(BuildControls)).toHaveLength(1);
+  });
+});
